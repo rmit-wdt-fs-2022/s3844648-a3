2b710a2d35bb96e9119849fa2cc77256f6fe320b
Jerin Kebha <S3844648@student.rmit.edu.au>
2022-02-07 21:26:46 +1100

    implemented Controller and view for displaying and creating staff

---
+++ s3844648-a3/Controllers/StaffController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using s3844648_a3.Data;
+using s3844648_a3.Models;
+
+namespace s3844648_a3.Controllers;
+
+public class StaffController : Controller
+{
+    private readonly MyContext _context;
+
+    public StaffController(MyContext context) => _context = context;
+
+    public IActionResult Index() => View(_context.Staff.ToList());
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Staff staff)
+    {
+        //validation
+        if (_context.Staff.Any(x => x.StaffID == staff.StaffID))
+            ModelState.AddModelError(nameof(staff.StaffID), "Staff with this ID already exists");
+        if (!ModelState.IsValid)
+            return View();
+
+        //insert Staff
+        _context.Staff.Add(new Staff
+        {
+            StaffID = staff.StaffID,
+            FirstName = staff.FirstName,
+            LastName = staff.LastName,
+            Email = staff.Email,
+            MobilePhone = staff.MobilePhone
+        });
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+}
--- s3844648-a3/Models/Staff.cs
+++ s3844648-a3/Models/Staff.cs
@@ -1,17 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace s3844648_a3.Models;
 public class Staff
 {
+    [Display(Name = "Staff ID")]
     [StringLength(6)]
-    [RegularExpression(@"^(^\e[0-9]{5})$", ErrorMessage = "Must start with e and be followed by 5 digits")]
-
+    [RegularExpression(@"^(^e[0-9]{5})$", ErrorMessage = "Must start with e and be followed by 5 digits")]
     public string StaffID { get; set; }
 
+    [Display(Name = "First Name")]
     [StringLength(30)]
     [RegularExpression(@"^(^[A-Z][a-z]+$)$", ErrorMessage = "Must start with an upper-case letter and only contain letters")]
     public string FirstName { get; set; }
 
+    [Display(Name = "Last Name")]
     [StringLength(30)]
     [RegularExpression(@"^(^[A-Z][a-z]+$)$", ErrorMessage = "Must start with an upper-case letter and only contain letters")]
     public string LastName { get; set; }
@@ -21,6 +24,7 @@ public class Staff
     [RegularExpression(@"^(^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$)$", ErrorMessage = "Must be a valid email address")]
     public string Email { get; set; }
 
+    [Display(Name = "Mobile Number")]
     [StringLength(10)]
     [RegularExpression(@"^(^04[0-9]{8})$", ErrorMessage = "Must start with 04 and be followed by 8 digits")]
     public string? MobilePhone { get; set; }
