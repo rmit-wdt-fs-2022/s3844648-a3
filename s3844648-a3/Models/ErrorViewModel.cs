fa40a702600b7f820201523155093466db44004c
Jerin Kebha <S3844648@student.rmit.edu.au>
2022-02-07 16:54:51 +1100

    Add project files.

---
+++ s3844648-a3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace s3844648_a3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
